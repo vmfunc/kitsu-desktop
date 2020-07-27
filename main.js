717777e7e54aabcb87399b61ff4ba6cf355543f7
Geshii <59031302+Geshii@users.noreply.github.com>
2020-07-27 14:16:50 +0200

    Updating to V1.0 Stable

---
+++ main.js
@@ -0,0 +1,85 @@
+// Modules to control application life and create native browser window
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+
+function createWindow () {
+  // Create the main window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    autoHideMenuBar: true,
+    titleBarStyle: 'customButtonsOnHover',
+    title: 'Kitsu! Desktop',
+    backgroundColor: '#FFF',
+    icon: 'images/logo.png',
+    show: false,
+    spellcheck: true, // Spell checker
+    devTools: false, //⚠️ Disabled in stable
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+// Creating the loading screen
+
+  let child = new BrowserWindow({
+     parent: mainWindow,
+     title: 'Loading...',
+     width: 300,
+     height: 350,
+      modal: true,
+       show: false,
+      autoHideMenuBar: true,
+      titleBarStyle: 'hidden',
+      skipTaskbar: true,
+      icon: 'images/logo.png',
+      alwaysOnTop: true,
+      frame: false,
+       })
+
+// Loading the screens
+
+  mainWindow.loadURL('https://kitsu.io')
+  child.loadFile('loading.html')
+
+// Start the loading screen
+
+  child.once('ready-to-show', () => {
+    child.show()
+  })
+
+// Then disable it and pop out the main window
+
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show()
+    child.hide()
+  })
+
+
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+  createWindow()
+  
+  app.on('activate', function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
